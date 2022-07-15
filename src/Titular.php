bf1ed23cd03d714499f55b104b5da263ec520900
SpartanWilhelm <clguilherme3@gmail.com>
2022-07-15 14:35:16 -0300

    Usando Heranca

--- src/Titular.php
+++ src/Titular.php
@@ -1,34 +1,15 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private $cpf;
     private $endereco;
-    private $nome;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
+        $this->endereco = $endereco;
     }
 
     public function recuperaEndereco(): Endereco
